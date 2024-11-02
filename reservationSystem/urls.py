f7484152ca960ccec904a051dfff5dcebd51669f
rzymski <83671265+rzymski@users.noreply.github.com>
2024-11-02 03:45:31 +0100

    czyszczenie smieci

--- reservationSystem/urls.py
+++ reservationSystem/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include, re_path
 from theme.views import change_theme
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
-from  django.views.static import serve
 urlpatterns = [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
     path('admin/', admin.site.urls),
